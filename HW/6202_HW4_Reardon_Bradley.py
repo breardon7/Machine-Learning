1118fa75bfc560b07462845d619983ab20fb65d1
breardon7 <70675472+breardon7@users.noreply.github.com>
2021-06-03 22:28:08 -0400

    hw4

--- HW/6202_HW4_Reardon_Bradley.py
+++ HW/6202_HW4_Reardon_Bradley.py
@@ -4,7 +4,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# i.
+
 def hardlim(x):
     if x < 0:
         return 0
@@ -12,24 +12,41 @@ def hardlim(x):
         return 1
 
 def ppn(p,t):
-    epoch = 100
+    epoch = 100000
     inputs = 2 # classes
     b = np.ones(1)
     w = np.ones(inputs)
-    epochs = 0
+    lr = 0.1
     #learning_rate = 0.2
     for j in range(epoch):
         for i in range(len(p)):
             n = np.dot(w,p[i]) + b
             a = hardlim(n)
             e = t[i] - a
-            w = w + e*p[i]
-            b = b + e
-        epochs += 1
-        E[j] =
+            w = w + lr*e*p[i]
+            b = b*lr + e
+    for i in range(len(p)):
+        plt.plot(p[i][0], p[i][1], 'rx' if (t[i] == 1) else 'bx')
+    x = np.linspace(0, 5)
+    m = -w[0]/w[1]
+    y = m*x + b
+    plt.plot(x,y)
+    plt.show()
 
 
+# Initial Train
 og_inputs = [np.array([1, 4]), np.array([1, 5]), np.array([2, 4]), np.array([2, 5]),
-             np.array([3, 1]), np.array([3, 2]), np.array([4, 1]), np.array([4, 2]),]
+             np.array([3, 1]), np.array([3, 2]), np.array([4, 1]), np.array([4, 2])]
 
 og_targets = np.array([0, 0, 0, 0, 1, 1, 1, 1])
+
+ppn(og_inputs, og_targets)
+
+# Second Train
+new_inputs = [np.array([1, 4]), np.array([1, 5]), np.array([2, 4]), np.array([2, 5]), np.array([1, 2]), np.array([3, 5]), np.array([3, 6]), np.array([4, 6]),
+             np.array([3, 1]), np.array([3, 2]), np.array([4, 1]), np.array([4, 2]), np.array([2, 1]), np.array([1, .5]), np.array([2.5, 2]), np.array([5, 4])]
+
+new_targets = np.array([0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1])
+
+ppn(new_inputs, new_targets)
+
--- HW/perceptron.py
+++ HW/perceptron.py
@@ -1,6 +1,46 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.linear_model import Perceptron
-import inspect
-
-print(inspect.getsource(Perceptron))
+def perceptron_learning_rule(input_vectors, targets):
+    no_of_inputs = 2  # rabbit or bears
+    weights = np.zeros(no_of_inputs)
+    bias = np.zeros(1)
+    epochs = 200  # iterations
+    epochs_count = 0
+    learning_rate = 0.1
+    error = []
+    for i in range(epochs):
+        for inputs, target in zip(input_vectors, targets):
+            rule = np.dot(inputs, weights) + bias  # n = w.p + b
+            if rule > 0:
+                activation = 1
+            else:
+                activation = 0
+            error.append(target - activation)  # e[i] = t[i] - a
+            weights += learning_rate * (target - activation) * inputs
+            bias += learning_rate * (target - activation)
+            epochs_count += 1
+    plt.plot(np.array([0, weights[0]]), np.array([weights[1], 0]), 'go-')
+    plt.xlabel("Weight on X")
+    plt.ylabel("Weight on Y")
+    plt.show()
+    m = -weights[0] / weights[1]
+    x = np.linspace(-5, 5)
+    y = m * x - bias / weights[1]
+    for i in range(len(targets)):
+        plt.plot(input_vectors[i][0], input_vectors[i][1], 'ro' if (targets[i] == 1.0) else 'bo')
+    plt.plot(x, y, 'g-')
+    plt.show()
+    print("The decision boundary is of weight: ", weights)
+# First Trial
+input_vectors = [np.array([1, 4]), np.array([1, 5]),
+                 np.array([2, 4]), np.array([2, 5]), np.array([3, 1]),
+                 np.array([3, 2]), np.array([4, 1]), np.array([4, 2])]
+targets = np.array([0, 0, 0, 0, 1, 1, 1, 1])
+perceptron_learning_rule(input_vectors, targets)
+# Second Trial
+input_vectors2= [np.array([1, 3]), np.array([1, 4]), np.array([1, 5]),
+                 np.array([2, 4]), np.array([2, 4]), np.array([2, 5]),
+                 np.array([3, 1]), np.array([3, 2]), np.array([3, 3]), np.array([3, 4]),
+                 np.array([4, 1]), np.array([4, 2]), np.array([4, 3]), np.array([4, 4])]
+targets2 = np.array([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1])
+perceptron_learning_rule(input_vectors2, targets2)
